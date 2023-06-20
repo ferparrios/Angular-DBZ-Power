86f1ddbbf4ab6aefc1983fff7937f52c7d3e6c91
Fernando Paredes Rios <fprios112@gmail.com>
2023-06-20 10:57:16 -0500

    uploading first files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  // templateUrl: 'name.component.html'
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetBy()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `,
+})
+export class CounterComponent {
+  constructor() {}
+
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetBy(): void {
+    this.counter = 10;
+  }
+}
